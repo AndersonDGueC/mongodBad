e29a439103bd865957e1347640df077c74bed109
anderson <adguerreroc@unal.edu.co>
2022-07-06 18:23:38 -0500

    creating schema

--- myApp.js
+++ myApp.js
@@ -1,4 +1,23 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+const {Schema} = mongoose;
 //console.log(process.env.MONGO_URI)
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const personSchema = new Schema({
+name: {type:String, require:true},
+age: Number,
+favoriteFoods: [String]
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+let userProgrammer = function(done) {
+	return new Person({
+	name: "FullStack Programmer",
+	age: "28",
+	favoriteFoods: ["Pizza", "HotDogs"]
+});
+	if(error) return done(error);
+	done(null, result);
+};
