b6e3968349b6394a234d42c3a7d391b52fa68f31
anderson <adguerreroc@unal.edu.co>
2022-07-06 20:08:27 -0500

    correccion requered

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ const {Schema} = mongoose;
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const personSchema = new Schema({
-name: {type:String, require:true},
+name: {type:String, required:true},
 age: Number,
 favoriteFoods: [String]
 });
